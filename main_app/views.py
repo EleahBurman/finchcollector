c45b7b3fd86944ecf2849adc2f702e7d4cabe0e2
EleahBurman <eleahjburman@gmail.com>
2023-05-31 09:05:37 -0400

    Renders an index page

--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,20 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+# Add new view
+def finch_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches })
+
+# Add the Cat class & list and view function below the imports
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, type, description, age):
+    self.name = name
+    self.type = type
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('CooCoo', 'Blue Finch', 'Chirpy in the morning', 3),
+  Finch('Margaret', 'Star Finch', 'Thinks she is a star', 1),
+]
