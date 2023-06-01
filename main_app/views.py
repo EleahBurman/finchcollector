9d3c5bf38f4b8f4063e25674b7bc5621f5a8acad
EleahBurman <eleahjburman@gmail.com>
2023-06-01 14:55:56 -0400

    Import cat model to index

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Finch
 
 # Create your views here.
 
@@ -10,17 +11,5 @@ def about(request):
 
 # Add new view
 def finch_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches })
-
-# Add the Cat class & list and view function below the imports
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, type, description, age):
-    self.name = name
-    self.type = type
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('CooCoo', 'Blue Finch', 'Chirpy in the morning', 3),
-  Finch('Margaret', 'Star Finch', 'Thinks she is a star', 1),
-]
