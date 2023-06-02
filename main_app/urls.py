8f4427c60a1aef2a2b995fa7a6965ec095caf9a9
EleahBurman <eleahjburman@gmail.com>
2023-06-01 20:53:50 -0400

    Add signup functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finch-delete'),
     path('finches/<int:finch_id>/add-feeding/', views.add_feeding, name='add-feeding'),
     path('toys/<int:finch_id>/assoc-toy/<int:toy_id>/', views.assoc_toy, name='assoc-toy'),
+    path('accounts/signup/', views.signup, name='signup'),
     path('toys/create/', views.ToyCreate.as_view(), name='toy-create'),
     path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy-detail'),
     path('toys/', views.ToyList.as_view(), name='toy-index'),
