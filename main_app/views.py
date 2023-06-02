774c24784bdceb00b3601532f2cfe4d3ff3715e2
EleahBurman <eleahjburman@gmail.com>
2023-06-01 20:38:00 -0400

    Login shows at home page

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from django.views.generic import ListView, DetailView
 from .models import Finch, Toy
 from .forms import FeedingForm
 
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
